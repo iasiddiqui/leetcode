f55d0c11efa11a7888a5c678d02e404148f466b8
Ishan Ahmad Siddiqui <115731094+iasiddiqui@users.noreply.github.com>
2025-10-09 21:03:01 +0530

    Update 26. Remove Duplicates from Sorted Array.js

--- 26. Remove Duplicates from Sorted Array.js
+++ 26. Remove Duplicates from Sorted Array.js
@@ -1,10 +1,10 @@
 /*
 Approach: Remove Duplicates from Sorted Array (Two-Pointer Technique)
 
-We use two pointers — one (i) to track the position of the last unique element and another (j) to iterate through the array. Whenever a new unique element is found, it’s placed next to the previous unique element, effectively overwriting duplicates in place. This ensures all unique elements are grouped at the start of the array while maintaining order.
+We use two pointers, one (i) to track the position of the last unique element and another (j) to iterate through the array. Whenever a new unique element is found, it’s placed next to the previous unique element, effectively overwriting duplicates in place. This ensures all unique elements are grouped at the start of the array while maintaining order.
 
-Time Complexity: O(n): traverses the array once.
-Space Complexity: O(1): uses constant extra space.
+Time Complexity: O(n) {traverses the array once.}
+Space Complexity: O(1) {uses constant extra space.}
 */
 
 /**
